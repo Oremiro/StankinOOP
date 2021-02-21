f7091294a49ad4d81d4a36b491942a3f9d6b19e2
Yaroslav <bondarev.yaroslav@outlook.com>
2021-02-21 23:52:42 +0300

    create mock projects for each lab

---
+++ Lab4.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Lab4.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
---
+++ Lab5.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Lab5.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
